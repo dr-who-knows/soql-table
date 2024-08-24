f5f3d09a516e75e2ee9c8764bfbd77b0ca22ac93
Valeriy Marchenko <v.marchenko@amarkets.com>
2024-08-24 13:56:16 +0200

    added query execution to component

--- force-app/main/default/lwc/soqlTable/soqlTable.js
+++ force-app/main/default/lwc/soqlTable/soqlTable.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import executeQuery from '@salesforce/apex/SOQLQueryController.executeQuery';
 
-export default class SoqlTable extends LightningElement {}
+export default class SoqlTable extends LightningElement {
+    soqlQuery = '';
+
+    handleQueryChange(event) {
+        this.soqlQuery = event.target.value;
+    }
+
+    handleExecuteQuery() {
+        executeQuery({ soql: this.soqlQuery })
+        .then(result => {
+            console.log(result);
+        })
+        .catch( error => {
+            console.log(error.body.message);
+        });
+    }
+}
